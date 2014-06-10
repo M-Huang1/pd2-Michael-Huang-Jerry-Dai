190074c67e49aca5c20053efcf2ece4f73ffc0b8
Michael Huang <mikester591@gmail.com>
2014-06-10 09:15:30 -0400

    Developed Map

--- Map.java
+++ Map.java
@@ -1,9 +1,25 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
 import java.util.Random;
 
-public class Map extends JPanel {
 
+public class Map extends JPanel {
+    public void  Reverse(Color[][] x,int y,int d){
+	for(int xx =0;xx<5;xx++){
+	    Color temp = x[y][xx];
+	    x[y][xx]= x[y][9-xx];
+	    x[y][9-xx]= temp;
+	}
+	for(int xx =0;xx<5;xx++){
+	    Color temp = x[d][xx];
+	    x[d][xx]= x[d][9-xx];
+	    x[d][9-xx]= temp;
+	}
+	
+    }
+	
     public static final Color Red = Color.RED;
     public static final Color Blue = Color.BLUE;
     public static final Color Green = Color.GREEN;
@@ -23,7 +39,7 @@ public class Map extends JPanel {
     private final JButton XD= new JButton("Button1");
     private final JButton XA= new JButton("Button2");
     private final JButton XC= new JButton("Button3");
-   
+    private  JLabel Tina = new JLabel("Tina is a person");
     public Map(){
         this.terrainGrid = new Color[NR][NC];
         int dd =0;
@@ -67,14 +83,43 @@ public class Map extends JPanel {
 		}
 	    }
 	}
+	Reverse(terrainGrid,2,6);
         int preferredWidth = NC *PIXELS;
         int preferredHeight = NR * PIXELS;
         setPreferredSize(new Dimension(preferredWidth, preferredHeight));
+	ButtonHandler BH = new ButtonHandler();
+	XD.addActionListener(BH);
+	
 	add(XD);
 	add(XA);
 	add(XC);
+	add(Tina);
     }
-
+    private class ButtonHandler implements ActionListener{
+	int count = 0;
+	public void actionPerformed(ActionEvent e){
+	    if(e.getSource()==XD){
+		Random rand = new Random();
+		
+		Color d = TERRAIN[rand.nextInt(4)];
+		if (d.equals(Color.RED)){
+		    Tina.setText("Red");
+		}
+		else if(d.equals(Color.YELLOW)){
+		    Tina.setText("Yellow");
+		}
+		else if(d.equals(Color.GREEN)){
+		    Tina.setText("Green");
+		}
+		else {
+		    Tina.setText("Blue");
+		}
+		
+		
+		
+	    }
+	}
+    }
     @Override
     public void paintComponent(Graphics g) {
    
@@ -84,7 +129,7 @@ public class Map extends JPanel {
    
         int rectWidth = getWidth() / NC;
         int rectHeight = getHeight() / NR;
-
+	
         for (int i = 0; i < NR; i++) {
             for (int j = 0; j < NC; j++) {
                
@@ -95,6 +140,15 @@ public class Map extends JPanel {
                 g.fillRect(x, y, rectWidth, rectHeight);
             }
         }
+	Ellipse2D p = new Ellipse2D.Double(0,0,20,20);
+	Ellipse2D p1 = new Ellipse2D.Double(0,40,20,20);
+	Ellipse2D p2 = new Ellipse2D.Double(40,0,20,20);
+	Ellipse2D p3 = new Ellipse2D.Double(40,40,20,20);
+	Graphics2D g2 = (Graphics2D)g;
+	g2.fill(p);
+	g2.fill(p1);
+	g2.fill(p2);
+	g2.fill(p3);
     }
 
     public static void main(String[] args) {
